dd06c78c7af5a986f67d48489c8ab13e4336765f
B1acK_BoroN <galatonovdenis@mail.ru>
2023-05-18 20:15:07 +0300

    add div throth app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+const app = (
+    <div>
+        <h1 id="text">Hello App</h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
+            reiciendis enim voluptatem, dolor quibusdam ab illo! Nobis eaque
+            assumenda rem blanditiis corrupti natus eius tenetur cumque ex aut
+            quo, temporibus ut sed ab rerum dignissimos recusandae quisquam
+            doloremque praesentium dolores autem dolore cum perferendis?
+            Pariatur dolor veritatis necessitatibus natus corporis.
+        </p>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
