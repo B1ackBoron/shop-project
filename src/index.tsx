8d0536679c84e1f1b654d82f2880e6972969415a
B1acK_BoroN <galatonovdenis@mail.ru>
2023-05-18 20:43:31 +0300

    added React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const app = (
-    <div>
-        <h1 id="text">Hello App</h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
-            reiciendis enim voluptatem, dolor quibusdam ab illo! Nobis eaque
-            assumenda rem blanditiis corrupti natus eius tenetur cumque ex aut
-            quo, temporibus ut sed ab rerum dignissimos recusandae quisquam
-            doloremque praesentium dolores autem dolore cum perferendis?
-            Pariatur dolor veritatis necessitatibus natus corporis.
-        </p>
-    </div>
-)
+const Title = () => {
+    return <h1>Hello App component</h1>
+}
+
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium corporis earum quae quod, eum impedit ducimus ullam
+                similique numquam sint eos vel ipsa quidem dolores, dolore
+                fugit, delectus doloribus molestiae?
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
+                ea error magni voluptate ab numquam alias, tenetur dolore quis,
+                deleniti magnam? Adipisci tempora, voluptas nobis ratione
+                impedit sit quod eveniet!
+            </p>
+        </React.Fragment>
+    )
+}
+const App = () => {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
