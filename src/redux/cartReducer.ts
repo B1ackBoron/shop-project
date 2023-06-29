68c8d414f84790068976fdd66b07338f2b3faad3
B1acK_BoroN <galatonovdenis@mail.ru>
2023-06-29 23:15:48 +0300

    remove initialState data from cartReducer

--- src/redux/cartReducer.ts
+++ src/redux/cartReducer.ts
@@ -5,10 +5,7 @@ type State = {
     [id: number]: number
 }
 
-const initialState: State = {
-    1: 2,
-    2: 2,
-}
+const initialState: State = {}
 
 export const cartSlice = createSlice({
     name: 'cart',
